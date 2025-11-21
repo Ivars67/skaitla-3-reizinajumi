d938e7d5b38ee5679d08b86a2b91a3ff86238ba8
Ivars67 <ivars.kristapsons2008@gmail.com>
2025-11-21 08:59:29 +0200

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+    vector<int> first_three_multiples(int a){
+    
+        vector<int> result;
+        
+        result.push_back(a*1);
+        result.push_back(a*2);
+        result.push_back(a*3);
+        
+        return result;
+    }
+    
+    int main(){
+        
+        vector<int> a;
+        a = first_three_multiples(7);
+        for(int i=0; i<a.size(); i++){
+            cout<<a[i]<<endl;
+        }
+        
+    }
