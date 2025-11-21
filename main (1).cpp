bb3d6aeb10bcb5d22fc590bc5b774c00020b1328
Ivars67 <ivars.kristapsons2008@gmail.com>
2025-11-21 09:00:00 +0200

    Update main (1).cpp

--- main (1).cpp
+++ main (1).cpp
@@ -19,7 +19,8 @@ using namespace std;
         vector<int> a;
         a = first_three_multiples(7);
         for(int i=0; i<a.size(); i++){
-            cout<<a[i]<<endl;
+            cout<<a[i]<<endl<<endl;
         }
         
+
     }
